5f4f36a313a9a39d348cc65bec0a6c182dad164a
jsuttner <jsuttner@stud.hs-bremen.de>
2014-03-27 09:28:55 +0100

    Initial Commit

---
+++ NFC/src/de/work/nfc/MainActivity.java
@@ -0,0 +1,37 @@
+package de.work.nfc;
+
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void onResume() {
+	    super.onResume();
+	    // create Intent
+	    Intent intent = new Intent();
+	    // Array mit den Messages
+	    NdefMessage[] msgs;
+	    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+	        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+	        if (rawMsgs != null) {
+	            msgs = new NdefMessage[rawMsgs.length];
+	            for (int i = 0; i < rawMsgs.length; i++) {
+	                msgs[i] = (NdefMessage) rawMsgs[i];
+	            }
+	        }
+	    }
+	    //process the msgs array
+	}
+
+}
